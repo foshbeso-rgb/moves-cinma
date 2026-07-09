e80cc1075631be7cb7c014ffeea5566bf7bcdd25
Fouad abbas <foshbeso@gmail.com>
2026-07-09 05:03:23 +0300

    Refactor import statements in index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,3 @@
-    import sys
-    import os
-    sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from app import app
 
-    from app import app 
-    application = app
+application = app
