652c1fb304adbdec14b1f26dea92ece1089d3264
Fouad abbas <foshbeso@gmail.com>
2026-07-09 08:38:02 +0300

    Update print statement from 'Hello' to 'Goodbye'

--- sections.py
+++ sections.py
@@ -370,19 +370,21 @@ def get_player(servers_html):
     .server-btn:hover {{ background:#333; }}
     </style>
     ''')
+
+    
 def get_servers(id, media_type, season=1, episode=1):
     if media_type == 'movie':
         return [
-            ('▶️ سيرفر 1 - SuperEmbed مترجم', f'https://multiembed.mov/?video_id={id}&tmdb=1&lang=ar&sublang=ar'), # ده مترجم
-            ('▶️ سيرفر 2 - VidSrc', f'https://vidsrc.to/embed/movie/{id}'),
-            ('▶️ سيرفر 3 - VidSrc.xyz', f'https://vidsrc.xyz/embed/movie/{id}'),
+            ('▶️ سيرفر 1 - Vidsrc.su مترجم', f'https://vidsrc.su/embed/movie/{id}/ar'), # ده فيه ترجمة
+            ('▶️ سيرفر 2 - SuperEmbed', f'https://multiembed.mov/?video_id={id}&tmdb=1&lang=ar&sublang=ar'),
+            ('▶️ سيرفر 3 - VidSrc.to', f'https://vidsrc.to/embed/movie/{id}'),
             ('⬇️ معلومات التحميل', f'https://www.themoviedb.org/movie/{id}')
         ]
     else:
         return [
-            ('▶️ سيرفر 1 - SuperEmbed مترجم', f'https://multiembed.mov/?video_id={id}&tmdb=1&s={season}&e={episode}&lang=ar&sublang=ar'), # ده مترجم
-            ('▶️ سيرفر 2 - VidSrc', f'https://vidsrc.to/embed/tv/{id}/{season}/{episode}'),
-            ('▶️ سيرفر 3 - VidSrc.xyz', f'https://vidsrc.xyz/embed/tv/{id}/{season}-{episode}'),
+            ('▶️ سيرفر 1 - Vidsrc.su مترجم', f'https://vidsrc.su/embed/tv/{id}/{season}/{episode}/ar'), # ده فيه ترجمة
+            ('▶️ سيرفر 2 - SuperEmbed', f'https://multiembed.mov/?video_id={id}&tmdb=1&s={season}&e={episode}&lang=ar&sublang=ar'),
+            ('▶️ سيرفر 3 - VidSrc.to', f'https://vidsrc.to/embed/tv/{id}/{season}/{episode}'),
             ('⬇️ معلومات التحميل', f'https://www.themoviedb.org/tv/{id}')
         ]
 
